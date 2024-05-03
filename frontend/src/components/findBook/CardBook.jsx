f1df87f867779caae7e0566e80fe23056210d07d
Pablo Axel Flores Bautista <pabloaxelcsr@gmail.com>
2024-05-02 21:34:41 -0600

    get your books upgraded

--- frontend/src/components/findBook/CardBook.jsx
+++ frontend/src/components/findBook/CardBook.jsx
@@ -1,34 +1,36 @@
-import React from 'react';
-import {
-    Card, Typography,
-    Stack, Button
-} from '@mui/material';
+import React from "react";
+import { Card, Typography, Stack, Button } from "@mui/material";
 
 export default function CardBook({
-    title,
-    author,
-    description,
-    imagePath,
-    buyLink
+  title,
+  author,
+  description,
+  imagePath,
+  buyLink,
 }) {
-    return (
-        <Card>
-            <Stack direction='row'>
-                <Stack>
+  return (
+    <Card sx={{ m: 2, boxShadow: 3 }}>
+      <Stack direction="row">
+        <Stack>
+          <Typography variant="h4" sx={{ color: "#333", fontSize: "20px", margin:'2rem', textAlign:'center'}}>
+            {title}
+          </Typography>
+          <Typography variant="h5" sx={{ color: "#666", fontSize: "18px", textAlign:'center' }}>
+            {author}
+          </Typography>
+        </Stack>
+        <img src={imagePath} style={{ width: '100px', height: '150px', objectFit: 'cover' }} />
 
-                    <Typography variant='h4'>
-                        {title}
-                    </Typography>
-                    <Typography variant='h5'>
-                        {author}
-                    </Typography>
-                </Stack>
-                <img src={imagePath} />
-                <Typography variant='h6'>
-                    {description}
-                </Typography>
-                <Button variant='contained'>Comprar</Button>
-            </Stack>
-        </Card>
-    )
+        <Typography variant="h6" sx={{ color: "#999", fontSize: "16px", margin:'2rem'}}>
+          {description}
+        </Typography>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "#007bff", color: "#fff" }}
+        >
+          Comprar
+        </Button>
+      </Stack>
+    </Card>
+  );
 }
